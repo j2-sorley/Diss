eb6ccf64e9f91aa37c2bf0f0f48bca7a2084ff4b
Josh <joshua2.sorley@live.uwe.ac.uk>
2024-01-18 13:50:01 +0000

    and the lord said let there be random light

--- Assets/lightAtt.cs
+++ Assets/lightAtt.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingleItemArray : MonoBehaviour
+public class lightAtt : MonoBehaviour
 {
 
-    public GameObject[] Items;
-    public Transform tr; 
-    int i; 
-
-
+    public Light light; 
     // Start is called before the first frame update
     void Start()
     {
-        i = Random.Range(0, Items.Length);
-        Instantiate(Items[i], tr.position, tr.rotation);
+        light.intensity = Random.Range(0.0f, 1.0f);
+        light.color = Random.ColorHSV(0f, 1f, 1f,1f,0.5f,1f); 
     }
 
     // Update is called once per frame
