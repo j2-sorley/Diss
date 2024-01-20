d584057737415ea70d172eb2caa0c935fc2931ba
Josh <joshua2.sorley@live.uwe.ac.uk>
2024-01-20 12:50:32 +0000

    Photos

--- Assets/lightAtt.cs
+++ Assets/lightAtt.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class lightAtt : MonoBehaviour
 {
 
-    public Light light; 
+    public Light light;
+    public Color[] colors;
+    private Color set;
+    public bool randomColor;
+    public bool setColor;
+    public bool randomIntensity; 
+   
     // Start is called before the first frame update
     void Start()
     {
-        light.intensity = Random.Range(0.0f, 1.0f);
-        light.color = Random.ColorHSV(0f, 1f, 1f,1f,0.5f,1f); 
+        set = colors[Random.Range(0, colors.Length)]; 
+        if (randomColor) {light.color = Random.ColorHSV(0f, 1f, 1f,1f,0.5f,1f);}
+        if (setColor) { light.color = set; }
+        if (randomIntensity) { light.intensity = Random.Range(0.0f, 1.0f); }
+        
     }
 
     // Update is called once per frame
